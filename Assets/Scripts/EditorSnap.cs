a3f611954cf54a3a2f5f6f7439d2ab00952fa736
caiogamedesign <caio.gamedesign@gmail.com>
2019-08-03 12:31:57 -0300

    Created snapping script for editor and implemented on quad cube

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    public const int POSITION_Y = 0;
+
+    [SerializeField] [Range(1, 20)] private int snapIncrement = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        SnapTransformPositionHorizontal();
+    }
+
+    private void SnapTransformPositionHorizontal()
+    {
+        int snapPosX = SnapPosition(transform.position.x);
+        int snapPosZ = SnapPosition(transform.position.z);
+
+        transform.position = new Vector3(snapPosX, POSITION_Y, snapPosZ);
+    }
+
+    private int SnapPosition(float position)
+    {
+        return Mathf.RoundToInt(position / snapIncrement) * snapIncrement;
+    }
+}
