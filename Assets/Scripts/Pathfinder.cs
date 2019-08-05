f244e0584278ba1feb1dd072fcceff75ff84b85e
caiogamedesign <caio.gamedesign@gmail.com>
2019-08-05 15:54:51 -0300

    Created script to garantee only one instance at any giver position

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+    void Start()
+    {
+        LoadBlocks();
+    }
+
+    private void LoadBlocks()
+    {
+        Waypoint[] waypoints = GetComponentsInChildren<Waypoint>();
+        foreach(Waypoint waypoint in waypoints)
+        {
+            print(waypoint.name);
+            Vector2Int gridPosition = waypoint.GetGridPosition();
+            bool isOverlapping = grid.ContainsKey(gridPosition);
+            if (isOverlapping)
+            {
+                Debug.LogWarning("Deleted Overlapping block " + waypoint + " with " + grid[gridPosition]);
+                Destroy(waypoint.gameObject);
+            }
+            else
+            {
+                grid.Add(gridPosition, waypoint);
+            }
+        }
+    }
+}
