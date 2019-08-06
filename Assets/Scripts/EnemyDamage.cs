7d43e9adfced3ebbf066179e911d9087cf23c6bd
caiogamedesign <caio.gamedesign@gmail.com>
2019-08-06 11:41:31 -0300

    Shooting Enemies only at a certain range

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] int hitPoints = 10;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hitPoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints--;
+        print("I'm Hit. Hitpoints: " + hitPoints);
+    }
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,37 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetEnemy;
+    [SerializeField] float attackRange = 20f;
+    [SerializeField] ParticleSystem bullets;
+
+    bool isShooting = false;
+
 
     private void LateUpdate()
     {
         objectToPan.LookAt(targetEnemy);
+
+        if (targetEnemy && Vector3.Distance(targetEnemy.transform.position, transform.position) <= attackRange)
+        {   
+            ShootEnemy();
+        }
+        else if (isShooting)
+        {
+            StopShooting();
+        }
+    }
+
+    private void StopShooting()
+    {
+        var em = bullets.emission;
+        em.enabled = false;
+        isShooting = false;
+    }
+
+    private void ShootEnemy()
+    {
+        var em = bullets.emission;
+        em.enabled = true;
+        isShooting = true;
     }
 }
